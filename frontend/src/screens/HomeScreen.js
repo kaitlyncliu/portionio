2679d36ccb716349de15233985a5093f24c230d5
kaitlyncliu <liu.kaitlync@gmail.com>
2022-09-04 00:48:13 -0400

    fix restaurant cards and scrolling

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -3,23 +3,22 @@ import axios from 'axios';
 import RestaurantCard from '../components/RestaurantCard';
 
 export default function HomeScreen() {
-  const [post, setPost] = React.useState(null);
-  const getResponse = async () => {
-    try {
-      const res = await axios.get('api/restaurants/1')
-      console.log(res.data)
-      setPost(res.data);
-    } catch(err) {
-      console.log(err)
-    }
-  }
-  getResponse()
-  return (
-    <RestaurantCard name={post.name}
-                    address={post.address}
-                    relativePortionSize={post.relativePortionSize}
-                    image={post.image}>
-    </RestaurantCard>
-   
-  );
-}; 
+	// const [post, setPost] = React.useState(null);
+	// const getResponse = async () => {
+	// 	try {
+	// 		const res = await axios.get('api/restaurants/1');
+	// 		console.log(res.data);
+	// 		setPost(res.data);
+	// 	} catch (err) {
+	// 		console.log(err);
+	// 	}
+	// };
+	// getResponse();
+	return (
+		// {name={post.name}
+		// address={post.address}
+		// relativePortionSize={post.relativePortionSize}
+		// image={post.image}}
+		<RestaurantCard />
+	);
+}
